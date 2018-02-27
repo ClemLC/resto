dc40ca76db12169a01152ec545a52c62af4f7dee
Clement <clementlecuillier@hotmail.fr>
2018-02-27 16:37:11 +0100

    carousel

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ## Tabs
 
- * ça marche en local
+* ça marche en local et sur heroku
 
 ## Carousel
 
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+* On y est pas tout a fait, tu peux aller d'une image à l'autre en cliquant sur précédent et suivant. Pas de boucles, de petits points, etc....
 
 le lien heroku : https://la-cuisine-au-beurre.herokuapp.com/
--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -16,4 +16,43 @@ $(document).ready(function(){
         $("#text_presentation").hide();
         $("#text_menu").hide();
     });
+
+    var $carousel = $('#carousel'),
+      $img = $('#carousel img'),
+      indexImg = $img.length - 1,
+      i = 0,
+      $currentImg = $img.eq(i);
+
+  $img.css('display', 'none');
+  $currentImg.css('display', 'block');
+
+  $("#next").click(function(){
+
+      i++;
+
+      if( i <= indexImg ){
+          $img.css('display', 'none');
+          $currentImg = $img.eq(i);
+          $currentImg.css('display', 'block');
+      }
+      else{
+          i = indexImg;
+      }
+
+  });
+
+  $("#prev").click(function(){
+
+      i--;
+
+      if( i >= 0 ){
+          $img.css('display', 'none');
+          $currentImg = $img.eq(i);
+          $currentImg.css('display', 'block');
+      }
+      else{
+          i = 0;
+      }
+
+  });
 });
